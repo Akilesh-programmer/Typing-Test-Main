edf9e45e25c2f7801a972535ad4319dd2e102387
Akilesh-programmer <akileshakileshs1234@gmail.com>
2023-09-09 10:13:02 +0530

    create test

---
+++ test.py
@@ -0,0 +1,15 @@
+import PyQt5.QtWidgets as qtw
+
+
+class MainWindow(qtw.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.show()
+
+
+app = qtw.QApplication([])
+mw = MainWindow()
+
+# Run the App
+app.exec_()
